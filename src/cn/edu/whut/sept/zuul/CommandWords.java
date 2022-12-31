def86697e9e05d416b2becb21fb933a32b7a7b1e
王煜泽 <2972079547@qq.com>
2023-01-01 00:01:28 +0800

    完成CommandWords类的注释

--- src/cn/edu/whut/sept/zuul/CommandWords.java
+++ src/cn/edu/whut/sept/zuul/CommandWords.java
@@ -6,11 +6,19 @@ public class CommandWords
             "go", "quit", "help"
     };
 
+    /**
+     * 初始化有效命令类
+     */
     public CommandWords()
     {
         // nothing to do at the moment...
     }
 
+    /**
+     * 验证玩家输入单词是否为有效指令
+     * @param aString 玩家输入的单词
+     * @return 如果输入的单词为有效指令，返回true，否则返回false
+     */
     public boolean isCommand(String aString)
     {
         for(int i = 0; i < validCommands.length; i++) {
@@ -20,6 +28,9 @@ public class CommandWords
         return false;
     }
 
+    /**
+     * 展示输出所有有效指令
+     */
     public void showAll()
     {
         for(String command: validCommands) {
