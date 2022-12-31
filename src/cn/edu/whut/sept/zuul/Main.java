49e290c9f576dbd36a89c326ef5e10ba76ce1507
王煜泽 <2972079547@qq.com>
2023-01-01 01:35:26 +0800

    完成Main类的注释

--- src/cn/edu/whut/sept/zuul/Main.java
+++ src/cn/edu/whut/sept/zuul/Main.java
@@ -2,6 +2,10 @@ package cn.edu.whut.sept.zuul;
 
 public class Main {
 
+    /**
+     * 启动类，初始化游戏实例并启动游戏
+     * @param args
+     */
     public static void main(String[] args) {
         Game game = new Game();
         game.play();
